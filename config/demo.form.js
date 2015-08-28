4c1cdfdf2f9304061950f7e09bfd299e25dc6860
rParslow <iorek@m4p.li>
2015-08-28 23:41:30 +0200

    help & License

--- config/demo.form.js
+++ config/demo.form.js
@@ -1,47 +1,38 @@
-
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Main configuration file for Payzen demo and prototyping examples
+//
+//   Usually demo.form.js is used to present your project and a very simmple example
+//    you can add more configuration files - Do not forget to add them in /index.html
+//
+//  - see sample.form.js  for more detailed instructions
+//
+// @license   The MIT License (MIT)  see /LICENSE
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var config =[
 
-
+// Banner
 {
-	"Menu": "demo",
-	"Text":"{large}{info}Exemple|L'exemple suivant est généré automatiquement avec pour seul paramètre le nom de la boutique."
-},
-{
-	"Shop":"DemoGO"
-},
-{
-    "Shop":"DemoGO",
-    "Anchor":"demo",
+ // Menu :
     "Menu": "demo",
-    "Title": "paiement en devise",
-    "Description":"Dans cet exemple le paramètre <code>vads_payment_cards</code> envoi à Payzen la carte que l'on souaite utiliser pour aller direcetement sur la page de paiement.<br>Montant de $vads_amount$<br>Vous pouvez tester 826:£ 840:$ 953=Franc Pacifique 978:€ …<br>La liste des langues est vonlontairement réduite à l'Anglais au Français et à l'Allemand.",
-    "helpText":"Dans cet exemple nous présentons la gestion des devises et des langues et des devises.", 
+    "Text":"{large}{info}Exemple|L'exemple suivant est généré automatiquement avec pour seul paramètre le nom de la boutique."
+},
 
-    "Help":"yes",
-    "Type":"hidden",
-    "Readonly":["vads_payment_cards"],
-    "Input":["vads_currency",'vads_amount',"vads_sub_currency",'vads_sub_amount'],
-
-    //vads_site_id : define via the shop parameter
-    //vads_trans_date  define automatically
-    "vads_ctx_mode":"TEST",
-    "vads_currency":"826",
-	"vads_sub_currency":"978",
-    "vads_action_mode":"INTERACTIVE",
-    "vads_page_action":"PAYMENT",
-    "vads_payment_config":"SINGLE",
-    "vads_version":"V2",
-
-    "vads_amount":(Math.floor((Math.random() * 90) + 51)*10),
-	"vads_sub_amount":(Math.floor((Math.random() * 90) + 51)*10),
-
-    "vads_order_info" : "éè_è_ç_çà$*%¨¨^^&&",
-
-    "ButtonText":"Moyens de paiment",
-    "Button":["VISA","MASTERCARD","VISA_ELECTRON","MAESTRO","<br><strong>Vous pouvez aussi payer en e-Chèques-Vacances et compléter par carte</strong>","E_CV"]
-}
-
+// Auto generated example - if 'Menu' isn't defined "demo" is used
+{
+    "Shop":"DemoGO"
+},
 
 ];
+
+// configuration end
+
+//
+//  Menu : define the page/url where the example will be displayed
+//
+//  Text : define a text box
+//   {large}      the text box will display on two columns - otherwise will be one column width
+//   {primary|success|info|warning|danger|…}  Bootstrap styles
+//   Title|text   html formating supported
+//
