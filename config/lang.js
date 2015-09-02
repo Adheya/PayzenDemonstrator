546f86d8c783faaa4f131f57ebde774e7abad2ea
rParslow <iorek@m4p.li>
2015-09-02 16:43:41 +0200

    header added

--- config/lang.js
+++ config/lang.js
@@ -1,6 +1,15 @@
-var noShopText = [];
-var cardsText = [];
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// Localisation file 
+//  you can add mode languages using 3166-1 for country code
+//
+// @license   The MIT License (MIT)  see /LICENSE
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var noShopText   = [];
+var cardsText    = [];
 noShopText["FR"] = "Boutique non répertoriée. <br>Configurez la boutique dans key.ini ou positionnez <br>var mode = 'javascript'; <br>dans form.js";
-cardsText["FR"] = "Survolez une icône pour afficher la valeur";
+cardsText["FR"]  = "Survolez une icône pour afficher la valeur";
 noShopText["EN"] = "Undefined shop. <br>Set the shop in the key.ini file or place<br>var mode = 'javascript'; <br>in the form.js file";
-cardsText["EN"] = "Place the mouse over an icon to display the value";
+cardsText["EN"]  = "Place the mouse over an icon to display the value";
+
+// End
