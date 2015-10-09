fbee1db22f6361efeb08e270c7778ce84e83654c
rParslow <iorek@m4p.li>
2015-10-09 19:45:54 +0200

    signature calculation for TEST and PRODUCTION mode

--- signature.php
+++ signature.php
@@ -1,45 +1,98 @@
 <?php
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Lyra-Network & contributor
+ *
+ * PayZen Demonstrator
+ * This project provide an easy way to test
+ * and demonstrate all the Payzen Capabilities
+ *
+ * AngularJS and PHP
+ *
+ * Author link : www.payzen.eu
+ *               www.lyra-network.com
+ *
+ * NOTICE OF LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @author    Parslow
+ * @copyright 2015 Lyra-Network and contributors
+ * @license   The MIT License (MIT)
+ * @version   X.X.X (revision xxxxxxx)
+*/
+
 header("Content-Type: text/plain");
 
+// Instantiate Class and variables
+//
 
-	
-$arg = (isset($_GET["arg"])) ? $_GET["arg"] : NULL;
-$name = (isset($_GET["name"])) ? $_GET["name"] : NULL;
+// Initialise var
+//
+$arg       = (isset($_GET["arg"]))  ? $_GET["arg"]  : NULL;
+$name      = (isset($_GET["name"])) ? $_GET["name"] : NULL;
 
-if (!$name){header("Location: http://demo.pzen.eu/d/formdemo.html#/Démo");}
+// die if unknown parameters
+//
+if (empty($name)) die("Unknown request");
 
-$arg = stripslashes($arg);
+// Initialize data
+//
 $ini_array = parse_ini_file("config/key.ini",true);
-$array = $ini_array[$name];
-$key = $array["key_test"];
-$tab = json_decode($arg,true);
-$tab["vads_site_id"]=$array["site_id"];
-ksort($tab);
+$array     = $ini_array[$name];
+$arg       = stripslashes($arg);
+$tab       = json_decode($arg,true);
+$key       = '';
 
-$text = "";
-
-foreach($tab as $k => $value){
- $text.= $value;
- $text.= "+";
+// Get key from parameters
+//
+if ($tab['vads_ctx_mode']=="TEST") {
+  $key = $array["key_test"];
+  $array["key"] = $array["key_test"];
+} elseif ($tab['vads_ctx_mode']=="PRODUCTION") {
+  $key = $array["key_prod"];
+  $array["key"] = $array["key_prod"];
 }
 
-$text.=$key;
-$sign = sha1($text);
-$array["sign"]=$sign;
-	
-$ret = json_encode($array);
-echo $ret;
-	
+// todo handle when no PRODUCTION/TEST key
 
-$subject = "[signature-payzen] name = $name ";
-$to      = 'demo.callback@pzen.eu';
-$to      = 'demo.callback@pzen.eu,lpenou@m4p.li';
-$headers = 'From: "CallBack10"<demo@pzen.eu>' . "\r\n" .
-     'Reply-To: demo+callback@pzen.eu' . "\r\n" .
-     'X-Mailer: PHP/' . phpversion();
-$m      = "tab  ".implode($tab,'+')."  $text arg = $arg ";
-
+// Calculate signature
 //
-  #$mailsent = mail($to, $subject, $m, $headers);
+$tab["vads_site_id"] = $array["site_id"];
+ksort($tab);
 
+// build string
+$kstring = "";
+foreach($tab as $k => $value) {
+ $kstring .= $value;
+ $kstring .= "+";
+}
+$kstring  .= $key;
+$sign     =  sha1($kstring);
+
+// Return result
+//
+$array["sign"] = $sign;
+
+// Fire the result as a json object
+echo   json_encode($array);
+
+// END
 ?>
